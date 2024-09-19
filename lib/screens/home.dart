9ce0f43adb24bec24cea7b98382b640f32bed91f
Error4403 <106513281+ThoNguyenIT@users.noreply.github.com>
2024-09-19 20:44:03 +0700

    test

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:buttons_tabbar/buttons_tabbar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,43 +12,132 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          ImageSlideshow(
-            width: double.infinity,
-            height: 405,
-            initialPage: 0,
-            indicatorColor: Colors.blue,
-            indicatorBackgroundColor: Colors.grey,
-            autoPlayInterval: 9000,
-            isLoop: true,
-            children: [
-              Image.asset(
-                'assets/images/home.png',
-                fit: BoxFit.cover,
-              ),
-              Image.asset(
-                'assets/images/home1.png',
-                fit: BoxFit.cover,
-              ),
-              Image.asset(
-                'assets/images/home2.png',
-                fit: BoxFit.cover,
-              ),
-            ],
-          ),
-          Positioned(
-            top: MediaQuery.of(context).padding.top,
-            left: 10,
-            child: IconButton(
-              icon: const Icon(Icons.menu, color: Colors.black, size: 30),
-              onPressed: () {
-                // Code to open a drawer or another menu
-              },
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: Stack(
+          children: [
+            ImageSlideshow(
+              width: double.infinity,
+              height: 350,
+              initialPage: 0,
+              indicatorColor: Colors.blue,
+              indicatorBackgroundColor: Colors.grey,
+              autoPlayInterval: 9000,
+              isLoop: true,
+              children: [
+                Image.asset(
+                  'assets/images/home.png',
+                  fit: BoxFit.cover,
+                ),
+                Image.asset(
+                  'assets/images/home1.png',
+                  fit: BoxFit.cover,
+                ),
+                Image.asset(
+                  'assets/images/home2.png',
+                  fit: BoxFit.cover,
+                ),
+              ],
             ),
-          ),
-        ],
+            Positioned(
+              top: MediaQuery.of(context).padding.top,
+              left: 10,
+              child: IconButton(
+                icon: const Icon(Icons.menu, color: Colors.black, size: 40),
+                onPressed: () {
+                  // Code to open a drawer or another menu
+                },
+              ),
+            ),
+            Positioned(
+              top: 120,
+              left: 25,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Opacity(
+                    opacity: 0.5,
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 35,
+                          height: 35,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/circle.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const Text(
+                          "성남호",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Opacity(
+                    opacity: 0.5,
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 35,
+                          height: 35,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/circle.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const Text(
+                          "판교호",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Opacity(
+                    opacity: 0.5,
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 35,
+                          height: 35,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/circle.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const Text(
+                          "분당호",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
